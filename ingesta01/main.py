dbfb2576f9bd13456d296db289a8d3e7aad4e481
hector <hectorcaballero1205@gmail.com>
2026-05-02 14:36:34 -0500

    add aws session token to .env.example

--- ingesta01/main.py
+++ ingesta01/main.py
@@ -15,6 +15,7 @@ s3 = boto3.client(
     "s3",
     aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+    aws_session_token=os.environ.get("AWS_SESSION_TOKEN", ""),
     region_name=os.environ["AWS_REGION"],
 )
 
